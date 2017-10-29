cfc4acaa9c9cf319fab4cc881bdbd803ba439674
Ekaterina Svetlakova <radistkatya@gmail.com>
2017-10-29 00:57:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-var = array[];
-
-function = addElementToBeginningOfArray(array, y){
-  newArray = array.unshift(y);
-  return newArray;
-}
-function = destructivelyAddElementToBeginningOfArray(array, y){
-array.unshift(y);
-return array
-}
+// var = array[];
+// 
+// function = addElementToBeginningOfArray(array, y){
+//   newArray = array.unshift(y);
+//   return newArray;
+// }
+// function = destructivelyAddElementToBeginningOfArray(array, y){
+// array.unshift(y);
+// return array
+// }
