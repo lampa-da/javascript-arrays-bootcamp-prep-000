e37bfeb3635c83288c3dce8d6a4c0618df4a5252
Ekaterina Svetlakova <radistkatya@gmail.com>
2017-10-29 01:27:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,8 +5,7 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
- array.unshift(element);
- return array
+  return array.unshift(element);
 }
 // function addElementToEndOfArray(array, element) {
 //   return [...array, element;
