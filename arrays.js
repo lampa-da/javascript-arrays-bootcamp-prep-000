0e64193d1acbc04c1d351420cd60887042b99fb7
Ekaterina Svetlakova <radistkatya@gmail.com>
2017-10-29 00:49:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var = chocolateBars[`snickers`, `hundred grand`, `kitkat`, `skittles`]
+
+var = array()
+
+function = addElementToBeginningOfArray(array, array[i]){
+  array.unshift(i+1)
+}
+function = destructivelyAddElementToBeginningOfArray(){
+  
+}
